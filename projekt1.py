b74de39a38eaade36f0a8446414df7d8216098fb
Em2510 <sokem1025@gmail.com>
2023-04-30 15:13:54 +0200

    zmiana elipsoidy

--- projekt1.py
+++ projekt1.py
@@ -1,4 +1,3 @@
-
 import argparse
 import numpy as np
 from math import *
@@ -11,6 +10,10 @@ class Transformacje:
             b - mała półoś elipsoidy - promień południkowy
             flat - spłaszczenie
             e2 - mimośród^2
+        + WGS84: https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+        + Inne powierzchnie odniesienia: https://en.wikibooks.org/wiki/PROJ.4#Spheroid
+        + Parametry planet: https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html
+        
         """
         if model == "wgs84":
             self.a = 6378137.0 # semimajor_axis
@@ -18,9 +21,9 @@ class Transformacje:
         elif model == "grs80":
             self.a = 6378137.0
             self.b = 6356752.31414036
-        elif model == "mars":
-            self.a = 3396900.0
-            self.b = 3376097.80585952
+        elif model == "kras":
+            self.a = 6378245.0
+            self.b = 6356863.01877305
         else:
             raise NotImplementedError(f"{model} model not implemented")
         self.flat = (self.a - self.b) / self.a
@@ -29,6 +32,29 @@ class Transformacje:
         
 
     def xyz2flh(self, X, Y, Z, output = 'dec_degree'):
+        """
+        Algorytm Hirvonena - algorytm transformacji współrzędnych ortokartezjańskich (x, y, z)
+        na współrzędne geodezyjne długość szerokość i wysokośc elipsoidalna (phi, lam, h). Jest to proces iteracyjny. 
+        W wyniku 3-4-krotneej iteracji wyznaczenia wsp. phi można przeliczyć współrzędne z dokładnoscią ok 1 cm.     
+        
+        ----
+        Parameters:
+        X, Y, Z : FLOAT
+            współrzędne w układzie ortokartezjańskim,
+
+        ----
+
+        Returns:
+        fi
+            [stopnie dziesiętne] - szerokość geodezyjna
+        lambda
+            [stopnie dziesiętne] - długość geodezyjna.
+        h : TYPE
+            [metry] - wysokość elipsoidalna
+        output [STR] - optional, default
+            dec_degree - decimal degree
+            dms - degree, minutes, sec
+        """
         P = sqrt(X**2 + Y**2)
         f = np.arctan(Z/(P*(1 - self.e2)))
         
@@ -51,6 +77,20 @@ class Transformacje:
             raise NotImplementedError(f"{output} - output format not defined")
 
     def dms(self, x):
+        """
+        Konwertuje wartość kąta w radianach na stopnie minuty i sekundy
+        
+        ----
+        Prameters:
+        X : FLOAT
+            Wartość kąta w radianach.
+
+        ----
+
+        Returns:
+        output [STR]
+            Watość kąta w stoniach minutach i sekundach 
+        """
         znak = ' '
         if x < 0:
             znak = '-'
@@ -62,6 +102,25 @@ class Transformacje:
         print(znak,d,m,s)
 
     def flh2xyz(self, f, l, h):
+        """
+        Funkcja odwrotna do algorytmu Hirvonena. Zamienia współrzędne geodezyjne (fi, lam, h)
+        na wpółrzędne ortokartezjańskie (X, Y, Z)  
+        ----
+        Parameters:
+        fi: FLOAT
+            [radiany] - szerokość geodezyjna
+        lam: FLOAT
+            [radiany] - długość geodezyjna
+        h: FLOAT
+            [metry] - wysokość elipsoidalna
+
+        ----
+
+        Returns:
+        X, Y, Z: FLOAT
+            [metry] - współrzędne ortokartezjańskie
+        output [STR] optional, default
+        """
         N = self.a / np.sqrt(1 - self.e2 * sin(f)**2)
         X = (N + h) * cos(f) * cos(l)
         Y = (N + h) * cos(f) * sin(l)
@@ -69,6 +128,24 @@ class Transformacje:
         return(X, Y, Z)
 
     def fl2neu(self, X, Y, Z, X0, Y0, Z0):
+        """
+        Funkcja przedstawia transformację współrzędnych geocentrycznych odbiornika (X, Y, Z)
+        do układu topocentrycznego aby uzyskać współrzędne topocentryczne odbiornika.
+        Zadanie wykorzystuje także współrzędne geocentryczne anteny (X0, Y0, Z0).
+        ----
+        Parameters:
+        X, Y, Z : FLOAT
+            współrzędne geocentryczne odbiornika (satelitów),
+        X0, Y0, Z0 : FLOAT
+            współrzędne geocentryczne anteny,
+        ----
+
+        Returns:
+        n, e, u: FLOAT
+            współrzędne ortokartezjańskie
+        output [STR] optional, default
+
+        """
         f, l, h = self.xyz2flh(X,Y,Z)
         R = np.array([[-np.sin(f) * np.cos(l), -np.sin(l), np.cos(f) * np.cos(l)],
                   [-np.sin(f) * np.sin(l),  np.cos(l), np.cos(f) * np.sin(l)],
@@ -87,6 +164,30 @@ class Transformacje:
         return n, e, u
     
     def fl2pl1992(self, f, l, l0=radians(19), m0 = 0.9993):
+        """
+        Funkcja przedstawia transformację współrzędnych geodezyjnych (fi, lam) 
+        do układu współrzędnych 1992(X1992, Y1992), który oparty jest na odwzorowaniu Gaussa-Krugera.
+        Ze współrzędnych geodezyjnych zostają wyznaczone płaskie współrzędne kartezjańskie (Xgk, Ygk). 
+        Następnie współrzędne kartezjańskie przeliczone są do układu współrzędnych 1992
+        ----
+        Parameters:
+        fi: FLOAT
+            [radiany] - szerokość geodezyjna
+        lam: FLOAT
+            [radiany] - długość geodezyjna
+        l0: FLOAT
+            [radiany] - południk środkowy
+        m0: FLOAT
+            skala na południku środkowym 
+        ----
+
+        Returns:
+        X1992, y1992: FLOAT
+            Współrzędne w układzie współrzędnych 1992
+        output [STR] optional, default
+
+        +https://ewmapa.pl/dane/wytyczne_g-1.10.pdf
+        """
         b2 = self.a**2*(1 - self.e2)
         ep2 = (self.a**2 - b2)/b2
         dl = l - l0
@@ -102,10 +203,34 @@ class Transformacje:
         ygk = dl*N*cos(f)*(1+(dl**2/6)*cos(f)**2*(1 - t**2 + n2) + (dl**4/120)*cos(f)**4*(5 - 18*t**2 + t**4 + 14*n2 - 58*n2*t**2))
         x92 = xgk * m0 - 5300000
         y92 = ygk * m0 + 500000
-        return xgk,ygk,x92,y92
+        return x92,y92
 
     def fl2pl2000(self, f, l, m0= 0.999923):
-        #https://gis-support.pl/baza-wiedzy-2/podstawy-gis/uklady-wspolrzednych-w-praktyce/
+        """
+        Funkcja przedstawia transformację współrzędnych geodezyjnych (fi, lam) 
+        do układu współrzędnych 2000(X2000, Y2000), który oparty jest na odwzorowaniu Gaussa-Krugera.
+        Ze współrzędnych geodezyjnych zostają wyznaczone płaskie współrzędne kartezjańskie (Xgk, Ygk). 
+        Następnie współrzędne kartezjańskie przeliczone są do układu współrzędnych 2000.
+        ----
+        Parameters:
+        fi: FLOAT
+            [radiany] - szerokość geodezyjna
+        lam: FLOAT
+            [radiany] - długość geodezyjna
+        l0: FLOAT
+            [radiany] - południk środkowy
+        m0: FLOAT
+            skala na południku środkowym 
+        ----
+
+        Returns:
+        X2000, y2000: FLOAT
+            Współrzędne w układzie współrzędnych 2000
+        output [STR] optional, default
+
+        +https://gis-support.pl/baza-wiedzy-2/podstawy-gis/uklady-wspolrzednych-w-praktyce/
+        """
+       
         if l>=14.1400 and l<=16.5000:
             l0 = radians(15)
         elif l>16.5000 and l<19.5000:
@@ -129,13 +254,12 @@ class Transformacje:
         ygk = dl*N*cos(f)*(1+(dl**2/6)*cos(f)**2*(1 - t**2 + n2) + (dl**4/120)*cos(f)**4*(5 - 18*t**2 + t**4 + 14*n2 - 58*n2*t**2))
         x2000 = xgk * m0
         y2000 = ygk * m0 + (l0/3) * 1000000 + 500000
-        return xgk,ygk,x2000,y2000
+        return x2000,y2000
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='dane.txt')
-    parser.add_argument('--a', type=float, default=6378137.0)
-    parser.add_argument('--e2', type=float, default=0.00669437999014)
+    parser.add_argument('--model', type=str, default="wgs84", help='Model elipsoidy:"wgs84", "grs80", "kras"')
     args = parser.parse_args()
 
     transformacje = Transformacje()
@@ -156,13 +280,11 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='flh.txt')
-    parser.add_argument('--a', type=float, default=6378137.0)
-    parser.add_argument('--e2', type=float, default=0.00669437999014)
+    parser.add_argument('--model', type=str, default="wgs84", help='Model elipsoidy:"wgs84", "grs80", "kras"')
     args = parser.parse_args()
     
     transformacje = Transformacje()
-    transformacje.a = args.a
-    transformacje.e2 = args.e2
+
     dane = np.loadtxt(args.input)
 
     with open(args.input, 'r') as plik:
@@ -180,14 +302,11 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='flh.txt')
-    parser.add_argument('--a', type=float, default=6378137.0)
-    parser.add_argument('--e2', type=float, default=0.00669437999014)
-    args = parser.parse_args()
+    parser.add_argument('--model', type=str, default="wgs84", help='Model elipsoidy:"wgs84", "grs80", "kras"')
     FL = np.loadtxt(args.input, usecols=(0,1))
     
     transformacje = Transformacje()
-    transformacje.a = args.a
-    transformacje.e2 = args.e2
+
     dane = np.loadtxt(args.input)
     
     with open(args.input, 'r') as plik:
@@ -206,14 +325,12 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='flh.txt')
-    parser.add_argument('--a', type=float, default=6378137.0)
-    parser.add_argument('--e2', type=float, default=0.00669437999014)
+    parser.add_argument('--model', type=str, default="wgs84", help='Model elipsoidy:"wgs84", "grs80", "kras"')
     args = parser.parse_args()
     FL = np.loadtxt(args.input, usecols=(0,1))
     
     transformacje = Transformacje()
-    transformacje.a = args.a
-    transformacje.e2 = args.e2
+   
     dane = np.loadtxt(args.input)
     with open(args.input, 'r') as plik:
         lines = plik.readlines()
